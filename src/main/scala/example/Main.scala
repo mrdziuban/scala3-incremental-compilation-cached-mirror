16ca0f59dc2dccdeb2e5ef1b43f432a6cc8a844c
Matt Dziuban <mrdziuban@gmail.com>
2024-12-09 12:00:45 -0500

    Reproduce issue on Scala 3.6.2.

--- src/main/scala/example/Main.scala
+++ src/main/scala/example/Main.scala
@@ -1,10 +1,5 @@
 package example
 
-import scala.deriving.Mirror
-
 object Main {
-  def main(args: Array[String]): Unit = {
-    val m = summon[Mirror.ProductOf[Test]]
-    println("\n***** Fields: " + compiletime.constValueTuple[m.MirroredElemLabels] + "\n\n")
-  }
+  def main(args: Array[String]): Unit = println("***** Fields: " + DepsLabels.labels())
 }
